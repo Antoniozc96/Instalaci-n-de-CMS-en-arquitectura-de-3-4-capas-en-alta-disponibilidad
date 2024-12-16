40d3c19ed52e52c16400e0971442d3e601e14797
Antoniozc96 <zanqui.96@gmail.com>
2024-12-16 23:47:48 +0100

    Update balanceador.sh

--- balanceador.sh
+++ balanceador.sh
@@ -1,45 +1,29 @@
-#! /bin/bash
-# Configuración para evitar problemas con fechas en repositorios archivados
-echo 'Acquire::Check-Valid-Until "false";' | sudo tee /etc/apt/apt.conf.d/99no-check-valid-until
+#!/bin/bash
 
-# Actualización de paquetes
-sudo apt update && sudp apt upgrade
+# Actualizar repositorios e instalar nginx
+sudo apt-get update -y
+sudo apt-get install -y nginx
 
-# Instalación de nginx
-sudo apt install -y nginx
-
-# Creación de la configuración del balanceador
-configuracion="
-upstream servidoresweb {
-    server 10.0.10.101;
-    server 10.0.10.102;
+# Configuracion de Nginx como balanceador de carga
+cat <<EOF > /etc/nginx/sites-available/default
+upstream backend_servers {
+    server 192.168.10.10;
+    server 192.168.10.11;
 }
-    
+
 server {
-    listen      80;
-    server_name balanceador;
+    listen 80;
+    server_name localhost;
 
     location / {
-        proxy_redirect      off;
-        proxy_set_header    X-Real-IP \$remote_addr;
-        proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header    Host \$http_host;
-        proxy_pass          http://servidoresweb;
+        proxy_pass http://backend_servers;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
-}"
+}
 
-# Borrado del archivo default de sites-enabled
-if [ -f /etc/nginx/sites-enabled/default ]; then
-    sudo rm /etc/nginx/sites-enabled/default
-fi
+EOF
 
-# Creación del archivo de configuración del balanceador
-if [ ! -f /etc/nginx/conf.d/balanceo.conf ]; then
-    sudo touch /etc/nginx/conf.d/balanceo.conf
-
-    # Edición del archivo de configuración para el balanceador
-    echo "$configuracion" | sudo tee /etc/nginx/conf.d/balanceo.conf > /dev/null
-
-    # Reinicio del servicio
-    sudo systemctl restart nginx
-fi
+# Reiniciar nginx para aplicar cambios
+sudo systemctl restart nginx
