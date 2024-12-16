80b0d165e31856534d5074f3b1c9fd4b5e67261c
Antoniozc96 <zanqui.96@gmail.com>
2024-12-16 23:51:06 +0100

    Update nfs.sh

--- nfs.sh
+++ nfs.sh
@@ -1,49 +1,71 @@
 #!/bin/bash
-sudo apt update && sudp apt upgrade
-sleep 1
 
-# Instalación de nfs-kernel-server y PHP
-sudo apt install -y nfs-kernel-server nfs-common php7.4-fpm php7.4-imagick php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-ldap php7.4-mbstring php7.4-mysql php7.4-pgsql php7.4-ssh2 php7.4-sqlite3 php7.4-xml php7.4-zip
+# Actualizar repositorios e instalar NFS y PHP 7.4
+sudo apt-get update -y
+sudo apt-get install -y nfs-kernel-server php7.4 php7.4-fpm php7.4-mysql php7.4-gd php7.4-xml php7.4-mbstring php7.4-curl php7.4-zip php7.4-intl php7.4-ldap unzip
 
-# Creación del directorio compartido
-if [ ! -d /var/nfs/joomla ]; then
-    sudo mkdir -p /var/nfs/joomla
-    sudo chown -R nobody:nogroup /var/nfs/joomla
-    sudo chmod -R 755 /var/nfs/joomla
-    echo "/var/nfs/joomla     172.16.0.101(rw,sync,no_subtree_check) 172.16.0.102(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
-    sudo systemctl restart nfs-kernel-server
-fi
+# Crear carpeta compartida para OwnCloud y configurar permisos
+sudo mkdir -p /var/www/html
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
 
-# Configuración de php-fpm e instalación de Joomla
-if [ ! -f $HOME/.flag ]; then
-    # Creo la bandera para evitar que se vuelva a ejecutar la configuración.
-    sudo touch $HOME/.flag
-    sudo chmod +t $HOME/.flag
+# Configurar NFS para compartir la carpeta
+echo "/var/www/html 192.168.56.11(rw,sync,no_subtree_check)" >> /etc/exports
+echo "/var/www/html 192.168.56.10(rw,sync,no_subtree_check)" >> /etc/exports
 
-    # Editar la directiva listen de fpm
-    sudo sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 172.16.0.100:9000|' /etc/php/7.4/fpm/pool.d/www.conf
+# Reiniciar NFS para aplicar cambios
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
 
-    # Instalación de Joomla
-    sudo apt install -y wget
-    sudo apt-get install zip unzip
-    sudo wget https://downloads.joomla.org/cms/joomla4/4-0-0/Joomla_4-0-0-Stable-Full_Package.tar.gz
-    sudo tar -xzvf Joomla_4-0-0-Stable-Full_Package.tar.gz -C /var/nfs/joomla
+# Descargar y configurar OwnCloud
+cd /tmp
+wget https://download.owncloud.com/server/stable/owncloud-10.9.1.zip
+unzip owncloud-10.9.1.zip
+mv owncloud /var/www/html/
 
-    # Editar configuration.php
-    sudo cp /var/nfs/joomla/installation/configuration.php-dist /var/nfs/joomla/configuration.php
-    #sudo sed -i "s/public \$host\s*=.*/public \$host = '172.16.0.200';/" /var/nfs/joomla/configuration.php
-    #sudo sed -i "s/public \$user\s*=.*/public \$user = 'joomla_user';/" /var/nfs/joomla/configuration.php
-    #sudo sed -i "s/public \$password\s*=.*/public \$password = 'joomla1234';/" /var/nfs/joomla/configuration.php
-    #sudo sed -i "s/public \$db\s*=.*/public \$db = 'joomla_db';/" /var/nfs/joomla/configuration.php
+# Configurar permisos de OwnCloud
+sudo chown -R www-data:www-data /var/www/html/owncloud
+sudo chmod -R 755 /var/www/html/owncloud
 
-    # Transferir propiedad
-    sudo chown -R www-data:www-data /var/nfs/joomla
-    sudo chmod -R 755 /var/nfs/joomla
+# Crear archivo de configuración inicial para OwnCloud
+cat <<EOF > /var/www/html/owncloud/config/autoconfig.php
+<?php
+\$AUTOCONFIG = array(
+  "dbtype" => "mysql",
+  "dbname" => "owncloud",
+  "dbuser" => "owncloud",
+  "dbpassword" => "12345",
+  "dbhost" => "192.168.60.10",
+  "directory" => "/var/www/html/owncloud/data",
+  "adminlogin" => "antonio",
+  "adminpass" => "1234?"
+);
+EOF
 
-    # Reinicio del servicio
-    sudo systemctl restart php7.4-fpm
-fi
+# Modificar el archivo config.php para agregar los dominios de confianza
+echo "Añadiendo dominios de confianza a la configuración de OwnCloud..."
+php -r "
+  \$configFile = '/var/www/html/owncloud/config/config.php';
+  if (file_exists(\$configFile)) {
+    \$config = include(\$configFile);
+    \$config['trusted_domains'] = array(
+      'localhost',
+      'localhost:8080',
+      '192.168.10.10',
+      '192.168.10.11',
+      '192.168.10.12',
+    );
+    file_put_contents(\$configFile, '<?php return ' . var_export(\$config, true) . ';');
+  } else {
+    echo 'No se pudo encontrar el archivo config.php';
+  }
+"
 
-# Denegar el acceso a internet
+# Configuración de PHP-FPM para escuchar en la IP del servidor NFS
+sed -i 's/^listen = .*/listen = 192.168.10.12:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+
+# Reiniciar PHP-FPM
+sudo systemctl restart php7.4-fpm
+
+#Eliminar puerta de enlace por defecto de Vagrant
 sudo ip route del default
-
