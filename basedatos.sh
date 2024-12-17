f52c3a817afc3990d55edc5321351dee6b22b772
Antoniozc96 <zanqui.96@gmail.com>
2024-12-17 03:53:20 +0100

    Update basedatos.sh

--- basedatos.sh
+++ basedatos.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y
 sudo apt-get install -y mariadb-server
 
 # Configurar MariaDB para permitir acceso remoto desde los servidores web
-sed -i 's/bind-address.*/bind-address = 192.168.10.10/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/bind-address.*/bind-address = 192.168.30.10/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Reiniciar MariaDB
 sudo systemctl restart mariadb
@@ -13,8 +13,8 @@ sudo systemctl restart mariadb
 # Crear base de datos y usuario para OwnCloud
 mysql -u root <<EOF
 CREATE DATABASE owncloud;
-CREATE USER 'owncloud'@'192.168.10.%' IDENTIFIED BY '12345';
-GRANT ALL PRIVILEGES ON owncloud.* TO 'owncloud'@'192.168.10.%';
+CREATE USER 'owncloud'@'192.168.30.%' IDENTIFIED BY '12345';
+GRANT ALL PRIVILEGES ON owncloud.* TO 'owncloud'@'192.168.30.%';
 FLUSH PRIVILEGES;
 EOF
 
